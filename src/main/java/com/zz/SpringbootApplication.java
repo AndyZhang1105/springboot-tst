f44f96df29837c78d3cde35f1301aecef4328a8d
Andy <andy@chengwkj.com>
2020-05-31 18:42:06 +0800

    refine druid, add log4j

--- src/main/java/com/zz/SpringbootApplication.java
+++ src/main/java/com/zz/SpringbootApplication.java
@@ -2,13 +2,11 @@ package com.zz;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@ServletComponentScan
 @RestController
 public class SpringbootApplication {
 
