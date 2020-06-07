8278343cf40f521532aaa51ccbfee9e123cff11f
Andy <andy@chengwkj.com>
2020-06-07 16:11:56 +0800

    design mode, decorator 2

--- src/main/java/com/zz/tst/designmode/decorator/DecoratorPattern.java
+++ src/main/java/com/zz/tst/designmode/decorator/DecoratorPattern.java
@@ -1,5 +1,8 @@
 package com.zz.tst.designmode.decorator;
 
+/*
+ * 装饰（Decorator）模式的定义：指在不改变现有对象结构的情况下，动态地给该对象增加一些职责（即增加其额外功能）的模式，它属于对象结构型模式。
+ */
 public class DecoratorPattern {
 
     public static void main(String[] args) {
